a19782d9e40e549d2e65032825597b86c09ebabf
Brian Utterback <brian@utterback.org>
2022-09-20 20:03:27 -0400

    Enable the Data Log Manager

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -31,6 +32,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     this.robotContainer = new RobotContainer();
+
+    // Start up the DataLog Manager
+    DataLogManager.start();
   }
 
   /**
