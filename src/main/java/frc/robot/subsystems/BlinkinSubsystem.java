38443e4f60f702924a70f7fbda5f3dc60ac1c46f
Brian Utterback <brian@utterback.org>
2023-05-26 18:48:24 -0400

    Add missing javadoc info.

--- src/main/java/frc/robot/subsystems/BlinkinSubsystem.java
+++ src/main/java/frc/robot/subsystems/BlinkinSubsystem.java
@@ -10,6 +10,15 @@ import java.util.function.DoubleSupplier;
 /**
  * Subsystem for controlling the Blinkin LED driver via PWM.
  *
+ * <p>There are three modes of operation. One is where the caller sets the value to be displayed.
+ * The second mode is a {@code Boolean} lambda expression is supplied when the Blinkin object is
+ * constructed, along with a value to use when the expression is true and one when it is false. The
+ * third mode is a {@code Double} lambda expression is supplied when the object is constructed and
+ * the value of this expression is used as the value.
+ *
+ * <p>In any of the three modes the Blinkin can be set to display the alliance color. And it can be
+ * enabled or disabled at any time.
+ *
  * <p>The color constants in this class only apply when the Blinkin is in the factory reset state.
  * To restore to factory reset do the following:
  *
@@ -47,27 +56,49 @@ public class BlinkinSubsystem extends SubsystemBase {
 
   /* Color Constants when in factory default state. */
 
+  /** Set color to hot pink. */
   public static final double HOT_PINK = 0.57;
+  /** Set color dark red. */
   public static final double DARK_RED = 0.59;
+  /** Set color dark red. */
   public static final double RED = 0.61;
+  /** Set color red-orange. */
   public static final double RED_ORANGE = 0.63;
+  /** Set color orange. */
   public static final double ORANGE = 0.65;
+  /** Set color gold. */
   public static final double GOLD = 0.67;
+  /** Set color yellow. */
   public static final double YELLOW = 0.69;
+  /** Set color lawn green. */
   public static final double LAWN_GREEN = 0.71;
+  /** Set color lime. */
   public static final double LIME = 0.73;
+  /** Set color dark green. */
   public static final double DARK_GREEN = 0.75;
+  /** Set color green. */
   public static final double GREEN = 0.77;
+  /** Set color blue-green. */
   public static final double BLUE_GREEN = 0.79;
+  /** Set color aqua. */
   public static final double AQUA = 0.81;
+  /** Set color sky blue. */
   public static final double SKY_BLUE = 0.83;
+  /** Set color dark blue. */
   public static final double DARK_BLUE = 0.85;
+  /** Set color blue. */
   public static final double BLUE = 0.87;
+  /** Set color blue-violet. */
   public static final double BLUE_VIOLET = 0.89;
+  /** Set color violet. */
   public static final double VIOLET = 0.91;
+  /** Set color white. */
   public static final double WHITE = 0.93;
+  /** Set color gray. */
   public static final double GRAY = 0.95;
+  /** Set color dark gray. */
   public static final double DARK_GRAY = 0.97;
+  /** Set color black. */
   public static final double BLACK = 0.99;
 
   private BlinkinSubsystem(
@@ -97,7 +128,14 @@ public class BlinkinSubsystem extends SubsystemBase {
   }
 
   /**
-   * Constructor that takes a PWM port number for the Blinkin LED driver.
+   * Constructor that takes a PWM port number for the Blinkin LED driver. In this mode the Blinkin's
+   * behavior is manually controlled by the caller. Use the {@code setValue()} method to set the
+   * colors displayed. The colors are only displayed when the the Blinkin is enabled using the
+   * {@code enable()} method. The Blinkin is initially disabled. The Blinkin can also be set to
+   * display the alliance color by calling the {@code displayAlliance()} method with the value
+   * {@code true}. The alliance display mode setting takes precedence over the current value
+   * setting. The alliance display mode is turned off by calling {@code displayAlliance()} with the
+   * value {@code false}.
    *
    * @param pwm the PWM for the Blinkin LED driver
    */
@@ -106,7 +144,9 @@ public class BlinkinSubsystem extends SubsystemBase {
   }
 
   /**
-   * Constructor that takes a Boolean lambda expression and two integers.
+   * Constructor that takes a Boolean lambda expression and two integers. The values must be in the
+   * rage -1.0 to 1.0. You may use the provided color constants if the Blinkin is in the factory
+   * default state.
    *
    * @param pwm the PWM for the Blinkin LED driver
    * @param condition the Boolean lambda expression to determine which integer value to set
@@ -118,7 +158,8 @@ public class BlinkinSubsystem extends SubsystemBase {
   }
 
   /**
-   * Constructor that takes an double lambda expression to set the value each cycle.
+   * Constructor that takes an double lambda expression to set the value each cycle. The values
+   * returned must be in the range -1.0 to 1.0.
    *
    * @param pwm the PWM for the Blinkin LED driver
    * @param valueFunc the double lambda expression to set the value each cycle
@@ -128,7 +169,9 @@ public class BlinkinSubsystem extends SubsystemBase {
   }
 
   /**
-   * Sets the value of the Blinkin LED driver.
+   * Sets the value to display of the Blinkin LED driver. The value is only displayed if the object
+   * is in the enabled state. The value is overridden by the {@code displayAlliance} setting, in
+   * which case the alliance color is displayed instead.
    *
    * @param value the value to set (0.0-1.0 range)
    */
@@ -201,14 +244,26 @@ public class BlinkinSubsystem extends SubsystemBase {
     }
   }
 
+  /** Closes the PWM port, allowing it to be reused by a new object. */
   public void close() {
     this.ledController.close();
   }
 
+  /**
+   * Gets the currently displayed value. This may differ from the current display value depending on
+   * the enabled state and whether {@code displayAlliance} is set to (@code true).
+   *
+   * @return The current value of the PWM port.
+   */
   public double getSpeed() {
     return (this.ledController.getSpeed());
   }
 
+  /**
+   * Set the display alliance state.
+   *
+   * @param display When true, set color to match alliance.
+   */
   public void setDisplayAlliance(boolean display) {
     this.displayAlliance = display;
   }
